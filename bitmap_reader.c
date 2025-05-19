df1c8419a52b2af0b1992c1a2cfa47f3a27065bc
Phone Htet Paing <phonehtetpaing990@gmail.com>
2025-05-20 03:49:52 +0530

    Read Bitmap image file and Read raw pixel data to Pixel Structure but not tested yet. And one thing that I have to remember is that I have to free the pointers and assign them to NULL

--- bitmap_reader.c
+++ bitmap_reader.c
@@ -1,7 +1,111 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+#pragma pack(push, 1)
+typedef struct 
 {
+    unsigned char signature[2];
+    unsigned int file_size;
+    unsigned short reserved01;
+    unsigned short reserved02;
+    unsigned int offset_pixel_array;
+
+    unsigned int header_size;
+    int width;
+    int height;
+    unsigned short planes;
+    unsigned short bit_per_pixel;
+    unsigned int compression;
+    unsigned int pixel_data_size;
+    unsigned int x_resolution;
+    unsigned int y_resolution;
+    unsigned int colors_palette;
+    unsigned int imp_colors;
+} BitmapHeader;
+#pragma pack(pop)
+
+typedef struct 
+{
+    unsigned char blue;
+    unsigned char green;
+    unsigned char red;
+} Pixel;
+
+
+int main(int argc, char *argv[])
+{
+    // check arg
+    if (argc != 3)
+    {
+        printf("Invalid Input\n");
+        return -11;
+    }
+
+    // Open the file
+    FILE *image_ptr = fopen(argv[1], "rb");
+    if (!image_ptr)
+    {
+        perror("Failed to Open the Image!\n");
+        return -1;
+    }
+
+    // Read the header
+    BitmapHeader header;
+    if (fread(&header, sizeof(BitmapHeader), 1, image_ptr) != 1)
+    {
+        perror("Failed to Read the Header!\n");
+        return -1;
+    }
+
+    // Verify BMP file
+    if (header.signature[0] != 'B' || header.signature[1] != 'M')
+    {
+        perror("Invalid Input! Not a Bitmap file(BMP)!\n");
+        return -1;
+    }
+
+    // Verify 24 bit uncompressed
+    if (header.compression != 0)
+    {
+        perror("Not a 24 bitmap file!\n");
+        return -1;
+    }
+
+    // Calculate Padding
+    int padding = (4 - ((3*header.width) % 4)) % 4;
+
+    // Allocate  memory for pixel data
+
+    Pixel **pixels = (Pixel**)(malloc(sizeof(Pixel) * header.height));
+    for (int i = 0; i < header.height; i++)
+    {
+        pixels[i] = (Pixel*)(malloc(sizeof(Pixel) * header.width));
+    }
+
+    // Read raw pixel dat
+    for (int y = 0; y < header.height; y++)
+    {
+        for (int x = 0; x < header.width; x++)
+        {
+            if (fread(&pixels[y][x], sizeof(Pixel), 1, image_ptr) != 1)
+            {
+                perror("Failed to Read Pixel data!\n");
+                // Clean up
+                for (int i = 0; i < header.width; i++) free(pixels[i]);
+                free(pixels);
+                pixels = NULL;
+                fclose(image_ptr);
+                return -2;
+            }
+        }
+        // Skip padding
+        fseek(image_ptr, padding, SEEK_CUR);
+    }
+        
+
+    // Processing Part
+
+
     printf("Fine!\n");
     return 0;
 }
